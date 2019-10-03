9ad43d79b95ae956e758975cfb0a3377007798e1
kristofferandreasen <kristofferaandreasen@gmail.com>
2019-10-03 14:56:29 +0200

    removed projects

--- 5-connect-to-and-consume-azure-services-and-third-party-services/azure-api-management-samples/delegation/ContosoWebApplication/ContosoWebApplication/Scripts/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="jquery-3.3.1.js" />
-/// <reference path="bootstrap.js" />
-/// <reference path="respond.js" />
